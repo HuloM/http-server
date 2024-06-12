6660b602dd03034261cc4def99c4b5da02b34669
HuloM <matthie.l.dubray@gmail.com>
2024-06-11 21:28:37 -0700

    http response 200 OK

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
     server_socket.accept() # wait for client
 
 
